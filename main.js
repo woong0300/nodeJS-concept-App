20341ecb102119065f4a36dd14216843558fa29c
woong0300 <woong0300@naver.com>
2020-03-26 20:05:58 +0900

    ExpressJS Middleware Concepts:Using & Making Middleware

--- main.js
+++ main.js
@@ -5,60 +5,75 @@ var template = require("./lib/template.js");
 var path = require("path");
 var qs = require("querystring");
 var sanitizeHtml = require("sanitize-html");
+var bodyparser = require("body-parser");
+var compression = require("compression");
+
+app.use(compression());
+//함수를 호출하면 미들웨어를 리턴하도록 되어있고 app.use를 통해 장착된다.
+//gzip으로 인코딩하고 해제하면서 사용한다.
+//압축, 해제보다 네트워크 전송비용이 더 많이 들어서 이게 훨 좋다.
+
+app.use(bodyparser.urlencoded({ extended: false }));
+//use의 안에 미들웨어가 들어오게 된다.
+//내부적으로 사용자가 전송한 post데이터를 내부적으로 분석해서
+//내 app에서 사용할 때, 내 앱의 콜백을 호출하도록 약속되어있다.
+
+//내 미들웨어 만들기
+app.get("*", function(request, response, next) {
+  fs.readdir("./data", function(err, filelist) {
+    request.list = filelist;
+    next();
+  });
+});
 
 // 라우팅
 //get : route, toutung을 해주는 함수이다.
 //app.get("/", (req, res) => res.send("Hello World!"));
 //위와 아래 코드는 동일한 작용을 하는 것이다.
 app.get("/", function(request, response) {
-  fs.readdir("./data", function(error, filelist) {
-    var title = "Welcome";
-    var description = "Hello, Node.js";
-    var list = template.list(filelist);
-    var html = template.HTML(
-      title,
-      list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    response.send(html);
-  });
+  var title = "Welcome";
+  var description = "Hello, Node.js";
+  var list = template.list(request.list);
+  var html = template.HTML(
+    title,
+    list,
+    `<h2>${title}</h2>${description}`,
+    `<a href="/create">create</a>`
+  );
+  response.send(html);
 });
 
 app.get("/page/:pageId", function(request, response) {
-  fs.readdir("./data", function(error, filelist) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"]
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
+    var title = request.params.pageId;
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ["h1"]
+    });
+    var list = template.list(request.list);
+    var html = template.HTML(
+      sanitizedTitle,
+      list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      ` <a href="/create">create</a>
           <a href="/update/${sanitizedTitle}">update</a>
           <form action="/delete_process" method="post">
             <input type="hidden" name="id" value="${sanitizedTitle}">
             <input type="submit" value="delete">
           </form>`
-      );
-      response.send(html);
-    });
+    );
+    response.send(html);
   });
 });
 
 app.get("/create", function(request, response) {
-  fs.readdir("./data", function(error, filelist) {
-    var title = "WEB - create";
-    var list = template.list(filelist);
-    var html = template.HTML(
-      title,
-      list,
-      `
+  var title = "WEB - create";
+  var list = template.list(request.list);
+  var html = template.HTML(
+    title,
+    list,
+    `
       <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -69,13 +84,21 @@ app.get("/create", function(request, response) {
         </p>
       </form>
     `,
-      ""
-    );
-    response.send(html);
-  });
+    ""
+  );
+  response.send(html);
 });
 
 app.post("/create_process", function(request, response) {
+  //body-parser라는 미들웨어를 통해 간단한게 처리된다.
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, "utf8", function(err) {
+    response.writeHead(302, { Location: `/?id=${title}` });
+    response.end();
+  });
+  /*
   var body = "";
   request.on("data", function(data) {
     body = body + data;
@@ -89,18 +112,18 @@ app.post("/create_process", function(request, response) {
       response.end();
     });
   });
+  */
 });
 
 app.get("/update/:pageId", function(request, response) {
-  fs.readdir("./data", function(error, filelist) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(
-        title,
-        list,
-        `
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, "utf8", function(err, description) {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(
+      title,
+      list,
+      `
         <form action="/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -112,44 +135,31 @@ app.get("/update/:pageId", function(request, response) {
           </p>
         </form>
         `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      //위의 코드에서 /update?id=${title} 부분은 /update/${title}로 수정 되어야 하는 버그입니다.
-      response.send(html);
-    });
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    //위의 코드에서 /update?id=${title} 부분은 /update/${title}로 수정 되어야 하는 버그입니다.
+    response.send(html);
   });
 });
 
 app.post("/update_process", function(request, response) {
-  var body = "";
-  request.on("data", function(data) {
-    body = body + data;
-  });
-  request.on("end", function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error) {
-      fs.writeFile(`data/${title}`, description, "utf8", function(err) {
-        response.redirect(`/?id=${title}`);
-      });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error) {
+    fs.writeFile(`data/${title}`, description, "utf8", function(err) {
+      response.redirect(`/?id=${title}`);
     });
   });
 });
 
 app.post("/delete_process", function(request, response) {
-  var body = "";
-  request.on("data", function(data) {
-    body = body + data;
-  });
-  request.on("end", function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(err) {
-      response.redirect("/");
-    });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(err) {
+    response.redirect("/");
   });
 });
 //마찬기지로 위와 아래 코드가 동일한 것이다.
